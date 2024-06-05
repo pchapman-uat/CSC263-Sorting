0776e211db571c0625d946a55573cdd09d3ad575
Preston Chapman <pchapman82070@uat.edu>
2024-06-05 12:52:27 -0700

    Add Selection Sort

--- Main.java
+++ Main.java
@@ -1,5 +1,25 @@
 public class Main {
+    private static Integer[] selectionSort(Integer... numbers){
+        int len = numbers.length;
+
+        for(int i=0; i<len-1;i++){
+            int minIndex = i;
+            for(int j=i+1;j<len;j++){
+                if(numbers[j]<numbers[minIndex]) minIndex = j;
+            }
+
+            int tmp = numbers[minIndex];
+            numbers[minIndex] = numbers[i];
+            numbers[i] = tmp;
+        }
+
+        return numbers;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Integer[] numbers = selectionSort(5,6,7,1,3,76,7,52);
+        for (Integer integer : numbers) {
+            System.out.println(integer);
+        }
     }
 }
