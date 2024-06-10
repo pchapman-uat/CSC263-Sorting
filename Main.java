0381b985691016803bb560d4c33018130a026d85
Preston Chapman <pchapman82070@uat.edu>
2024-06-10 13:05:46 -0700

    Added Merge Sort

--- Main.java
+++ Main.java
@@ -31,6 +31,51 @@ public class Main {
 
         return numbers;
     }
+
+    private static int[] mergeSort(Integer left, Integer right, int... numbers){
+        if(left == null) left = 0;
+        if(right == null) right = numbers.length -1;
+        if(left<right) {
+            int middle = (left+right)/2;
+            mergeSort(left, middle, numbers);
+            mergeSort(middle+1, right, numbers);
+
+            merge(left, middle, right, numbers);
+        }
+        return numbers;
+    }
+
+    private static int[] merge(int left, int middle, int right, int... numbers){
+        int n1 = middle - left + 1;
+        int n2 = right - middle;
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+        
+        System.arraycopy(numbers, left, L, 0, n1);
+        System.arraycopy(numbers, middle+1, R, 0, n2);
+
+        int i=0, j=0, k=left;
+        while(i<n1 && j<n2){
+            if(L[i] <= R[j]){
+                numbers[k] = L[i];
+                i++;
+            } else {
+                numbers[k] = R[j];
+                j++;
+            } k++;
+        }
+        while(i<n1){
+            numbers[k] = L[i];
+            i++;
+            k++;
+        }
+        while (j<n2) {
+            numbers[k] = R[j];
+            j++;
+            k++;
+        }
+        return numbers;
+    }
     public static void main(String[] args) {
         Integer[] numbers = selectionSort(5,6,7,1,3,76,7,52);
         for (Integer integer : numbers) {
@@ -39,6 +84,12 @@ public class Main {
         System.out.println("Insertion Sort");
         Integer[] numbers2 = insertionSort(5,424,5,15,12,66,24,42,4,1);
         for (Integer integer : numbers2) {
+            System.out.println(integer);
+        }
+
+        System.out.println("Merge Sort");
+        int[] numbers3 = mergeSort(null, null, 55,55,41,24,3,441,22,45,66,6,2,33,1,2);
+        for(Integer integer : numbers3){
             System.out.println(integer);
         }
     }
