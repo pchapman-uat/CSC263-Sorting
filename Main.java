7bb8096927a62cc5d3f457ef7bfae5859dc0d46a
Preston Chapman <pchapman82070@uat.edu>
2024-06-10 12:35:15 -0700

    Add Insertion Sort

--- Main.java
+++ Main.java
@@ -16,9 +16,29 @@ public class Main {
         return numbers;
     }
 
+    private static Integer[] insertionSort(Integer... numbers){
+
+        for (int i = 0; i < numbers.length; i++) {
+            int key = numbers[i];
+            int j = i-1;
+            
+            while(j>=0 && numbers[j]> key){
+                numbers[j+1] = numbers[j];
+                j -= 1;
+            }
+            numbers[j+1] = key;
+        }
+
+        return numbers;
+    }
     public static void main(String[] args) {
         Integer[] numbers = selectionSort(5,6,7,1,3,76,7,52);
         for (Integer integer : numbers) {
+            System.out.println(integer);
+        }
+        System.out.println("Insertion Sort");
+        Integer[] numbers2 = insertionSort(5,424,5,15,12,66,24,42,4,1);
+        for (Integer integer : numbers2) {
             System.out.println(integer);
         }
     }
